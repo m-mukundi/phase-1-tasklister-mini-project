2a18529cf045440ee63b47fe9c0ceb8342ebb885
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-04-26 22:59:31 +0300

    Lab Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	// your code here
+	function buildToDo(task) {
+		const taskList = document.getElementById("tasks");
+		const taskName = document.createElement("li");
+		taskName.textContent = task;
+
+		taskList.append(taskName);
+	}
+
+	const form = document.querySelector("#create-task-form");
+
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+
+		const newTask = event.target["new-task-description"].value;
+
+		buildToDo(newTask);
+	});
 });
